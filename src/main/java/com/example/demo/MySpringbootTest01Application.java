1930e05ac2479fda382566d4e83741f5f02cdaf1
klwan703 <53215363+klwan703@users.noreply.github.com>
2023-04-25 22:07:49 +0800

    Initial commit
    Initial commit

---
+++ src/main/java/com/example/demo/MySpringbootTest01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootTest01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringbootTest01Application.class, args);
+	}
+
+}
